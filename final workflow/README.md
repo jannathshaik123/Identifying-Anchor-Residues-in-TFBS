29be8255d10efab98d6a0d75ec6e9708eb53a696
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2025-02-12 22:23:42 +0400

    outdated files

--- final workflow/README.md
+++
@@ -1,2 +0,0 @@
-1) Run the main_script.py
-2) Ensure you have Docker Desktop installed - check out this link (https://meme-suite.org/meme/doc/download.html)
--- final workflow/working_with_glob.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-
-pattern = 'binning_bin*_*/*.fasta'
-fasta_files = glob.glob(pattern)
-output_files = [os.path.join(os.path.basename(os.path.dirname(f)), os.path.basename(f)) for f in fasta_files]
-
-print(output_files)
